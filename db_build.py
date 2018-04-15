227aa9f1c68736a05f702ff32fb18d0e46788c5b
Constantina <polyccon@gmail.com>
2018-04-15 12:35:31 +0100

    adds users table in database schema

--- db_build.py
+++ db_build.py
@@ -8,11 +8,19 @@ def create_connection(db_file):
         conn = sqlite3.connect(db_file)
         print(sqlite3.version)
         c = conn.cursor()
+
+        c.execute(''' CREATE TABLE users
+                    (id INTEGER PRIMARY KEY,
+                    user VARCHAR(100) UNIQUE NOT NULL,
+                    password VARCHAR(100) NOT NULL);''')
         c.execute(''' CREATE TABLE todo
                     (id INTEGER PRIMARY KEY,
-                    user VARCHAR(100),
-                    task VARCHAR(300), complete INT);''')
-
+                    user VARCHAR(100) REFERENCES users(user) NOT NULL,
+                    task VARCHAR(300) NOT NULL,
+                    complete INT NOT NULL);''')
+        c.execute('''INSERT INTO users
+                    VALUES (1, 'user18081971',
+                    'Aphex');''')
         c.execute('''INSERT INTO todo
                     VALUES (1, 'user18081971',
                     'wash the dishes', 0);''')
