a3e88e1d8bba2577b3096519680f5ddac0bf98cc
Constantina <polyccon@gmail.com>
2018-04-21 12:53:49 +0100

    fixes bug storing todos as string representation in memory store

--- src/store.py
+++ src/store.py
@@ -36,31 +36,31 @@ UserStore.create('user18081971','Aphex')
 class TodoStore:
     @classmethod
     def get(cls, user_id):
-
         return MemoryStore.get("todo:%s" % user_id) or []
 
     @classmethod
     def add(cls, user_id, task):
         todos = TodoStore.get(user_id)
+        print ('todos', todos, type(todos))
         keys = ['id', 'description', 'complete']
         values = [str(len(todos)+1), task, False]
         todos.append(dict(zip(keys, values)))
-        MemoryStore.set("todo:%s"% user_id, "%s" % todos)
+        MemoryStore.set("todo:%s"% user_id, todos)
 
     @classmethod
-    def complete_task(cls, username, task_id):
+    def complete_task(cls, user_id, task_id):
         todos = TodoStore.get(user_id)
         for item in todos:
             if item["id"] == str(task_id):
                 item["complete"] == True
-                MemoryStore.set("todo:%s"% user_id, "%s"% todos)
+                MemoryStore.set("todo:%s"% user_id, todos)
                 break
 
     @classmethod
-    def delete_task(cls, username, task_id):
+    def delete_task(cls, user_id, task_id):
         todos = TodoStore.get(user_id)
         for item in todos:
             if item["id"] == str(task_id):
                 todos.remove(item)
-                MemoryStore.set("todo:%s"% user_id, "%s"%  todos)
+                MemoryStore.set("todo:%s"% user_id, todos)
                 break
