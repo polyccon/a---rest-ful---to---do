dc5a0ca4b212cb6e8e6902535221da97cd720587
Constantina <polyccon@gmail.com>
2018-04-21 12:52:53 +0100

    adds test to add endpoint

---
+++ tests/test_add.py
@@ -0,0 +1,25 @@
+import pytest
+import requests
+from src.views import *
+
+def test_login_works_and_add_a_todo_subsequently():
+    """Test if '/login' and subsequent call to '/add' endpoint returns 200
+        when auth details are correct
+    """
+    body1 = {
+        'username': 'user18081971',
+        'password': 'Aphex'
+    }
+    body2 = {
+        'task': 'clean my cds'
+    }
+    s = requests.session()
+
+    s.post('http://localhost:8000/login',
+                             json=body1)
+    response = s.post('http://localhost:8000/add',
+                             json=body2)
+
+    actual = response.status_code
+
+    assert actual == 200
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,19 +2,10 @@ import pytest
 import requests
 from src.views import *
 
-@pytest.mark.parametrize(
-    ("params", "expected"),
-    [
-        (['http://localhost:8000/add',
-                                {
-                                     'task': 'clean my cds'
-                                 }],
-        401)
-    ]
-)
-def test_is_equal(params, expected):
-    """Test if call to '/add' endpoint is authenticated
-    """
-    response = requests.session().post(url= params[0], json=params[1])
-    actual = response.status_code
-    assert actual == expected
+
+def test_when_not_logged_in_then_call_to_add_should_be_rejected():
+    """ Test if call to '/add' endpoint is authenticated """
+
+    response = requests.session().post(url='http://localhost:8000/add',
+                    json={'task': 'clean my cds'})
+    assert response.status_code == 401
